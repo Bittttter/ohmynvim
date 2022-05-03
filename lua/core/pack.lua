10dee08f454159933faf88eab72040aaf0bc6b67
Bitter <chancezyw@gmail.com>
2022-05-04 02:01:12 +0800

    feat: setup nvim-tree

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -1,12 +1,33 @@
 vim.cmd([[packadd packer.nvim]])
-require('packer').startup(function()
+require('packer').startup(function(use)
     use {
         'wbthomason/packer.nvim',
         event = 'VimEnter',
     }
 
+    -- 英语不好，但熟悉了帮助文档之后要卸掉哦:)
+    use {
+		'yianwillis/vimcdoc'
+    }
+
+    -- theme
     use {
         'projekt0n/github-nvim-theme',
         config = function() require('github-theme').setup() end
     }
+
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- optional, for file icon
+		},
+		config = function() require('nvim-tree').setup() end
+	}
 end)
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost ~/.config/nvim/lua/config/pack.lua so <afile> | PackerCompile
+  augroup end
+]])
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -75,16 +75,36 @@ _G.packer_plugins = {
     path = "/Users/bitter/.local/share/nvim/site/pack/packer/start/github-nvim-theme",
     url = "https://github.com/projekt0n/github-nvim-theme"
   },
+  ["nvim-tree.lua"] = {
+    config = { "\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14nvim-tree\frequire\0" },
+    loaded = true,
+    path = "/Users/bitter/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    url = "https://github.com/kyazdani42/nvim-tree.lua"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/Users/bitter/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
   ["packer.nvim"] = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
     path = "/Users/bitter/.local/share/nvim/site/pack/packer/opt/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
+  },
+  vimcdoc = {
+    loaded = true,
+    path = "/Users/bitter/.local/share/nvim/site/pack/packer/start/vimcdoc",
+    url = "https://github.com/yianwillis/vimcdoc"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
 -- Config for: github-nvim-theme
 time([[Config for github-nvim-theme]], true)
 try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\17github-theme\frequire\0", "config", "github-nvim-theme")
